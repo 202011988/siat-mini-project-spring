765f58cbf798d0e077cf6d2740ec6f0b1219c3c1
lee-donggyu <dlehdrb5509@naver.com>
2024-06-19 16:57:33 +0900

    fix: Remove Relation from project to task

--- src/main/java/com/spring/todo/domain/project/entity/Project.java
+++ src/main/java/com/spring/todo/domain/project/entity/Project.java
@@ -1,11 +1,8 @@
 package com.spring.todo.domain.project.entity;
 
-import com.spring.todo.domain.task.entity.Task;
 import com.spring.todo.domain.user.entity.User;
 import com.spring.todo.global.utill.BaseEntity;
 import jakarta.persistence.*;
-
-import java.util.Set;
 
 @Entity
 @Table(name = "Projects")
@@ -28,8 +25,8 @@ public class Project extends BaseEntity {
 //    @Column(nullable = false)
 //    private LocalDateTime createdAt;
 
-    @OneToMany(mappedBy = "project")
-    private Set<Task> tasks;
+//    @OneToMany(mappedBy = "project")
+//    private Set<Task> tasks; // 양방향 제거
 
     // Getters and Setters
 }
