88f7ec52c9db54690d71b8a9f2ad4683ff7016fe
lee-donggyu <dlehdrb5509@naver.com>
2024-06-23 23:46:46 +0900

    fix: add parameter of getStepsByTaskId

--- src/main/java/com/spring/todo/domain/step/controller/StepController.java
+++ src/main/java/com/spring/todo/domain/step/controller/StepController.java
@@ -7,13 +7,7 @@ import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/steps")
@@ -23,7 +17,7 @@ public class StepController {
 
     // get steps
     @GetMapping("/")
-    public ResponseEntity<List<StepDTO>> getStepsByTaskId(Long taskId) {
+    public ResponseEntity<List<StepDTO>> getStepsByTaskId(@RequestParam Long taskId) {
         List<StepDTO> stepDTOS = stepService.getSteps(taskId);
         return new ResponseEntity<>(stepDTOS, HttpStatus.OK);
     }
