3cb12def6885dff8f5c98eec3b6d28d2ce71908c
gntdtndls156 <gntodtndls156@gmail.com>
2024-06-23 21:45:54 +0900

    fix: url

--- src/main/java/com/spring/todo/domain/step/controller/StepController.java
+++ src/main/java/com/spring/todo/domain/step/controller/StepController.java
@@ -6,6 +6,7 @@ import com.spring.todo.domain.step.service.StepService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/step")
+@RequestMapping("/api/steps")
 @RequiredArgsConstructor
 public class StepController {
     private final StepService stepService;
@@ -23,6 +24,12 @@ public class StepController {
     public ResponseEntity<StepDTO> createTask(@RequestBody StepDTO stepDTO, @PathVariable Long taskId) {
         StepDTO createdStep = stepService.createTask(stepDTO, taskId);
         return new ResponseEntity<>(createdStep, HttpStatus.CREATED);
+    }
+
+    // delete
+    @DeleteMapping("/{stepId}")
+    public ResponseEntity<Void> deleteStep(@PathVariable Long stepId) {
+        return null;
     }
 
 }
